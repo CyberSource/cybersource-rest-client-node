cdc2e5d6708652285b62e9d3b46008b374a70acd
brianmc <bmgtech@gmail.com>
2018-10-31 11:13:46 -0700

    removed local copy of AuthSDK

--- AuthenticationSDK/src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    Authorization: require('./com/cybersource/authentication/core/Authorization.js'),
-    MerchantConfig: require('./com/cybersource/authentication/core/MerchantConfig.js'),
-    Logger: require('./com/cybersource/authentication/util/Logger.js'),
-    Constants: require('./com/cybersource/authentication/util/Constants.js'),
-    PayloadDigest: require('./com/cybersource/authentication/payloadDigest/DigestGenerator.js')
-};
