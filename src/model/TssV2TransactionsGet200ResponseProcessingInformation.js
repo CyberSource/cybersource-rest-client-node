39e5e748dd86f1ac83e9399b76f95786f46abdb5
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2020-11-28 19:55:10 +0530

    Updates for December 2020 Release

--- src/model/TssV2TransactionsGet200ResponseProcessingInformation.js
+++ src/model/TssV2TransactionsGet200ResponseProcessingInformation.js
@@ -89,7 +89,7 @@
   }
 
   /**
-   * Type of digital payment solution for the transaction. Possible Values:   - `visacheckout`: Visa Checkout. This value is required for Visa Checkout transactions. For details, see `payment_solution` field description in [Visa Checkout Using the SCMP API.](https://apps.cybersource.com/library/documentation/dev_guides/VCO_SCMP_API/html/)  - `001`: Apple Pay.  - `004`: Cybersource In-App Solution.  - `005`: Masterpass. This value is required for Masterpass transactions on OmniPay Direct. For details, see \"Masterpass\" in the [Credit Card Services Using the SCMP API Guide.](https://apps.cybersource.com/library/documentation/dev_guides/CC_Svcs_SCMP_API/html/)  - `006`: Android Pay.  - `007`: Chase Pay.  - `008`: Samsung Pay.  - `012`: Google Pay. 
+   * Type of digital payment solution for the transaction.  
    * @member {String} paymentSolution
    */
   exports.prototype['paymentSolution'] = undefined;
