5b58fcd431a5f1a9c5a59ae0f220f11cf56031c2
Bansal <gaubansa@visa.com>
2024-10-01 13:14:18 +0530

    adding utility for MLE in client

---
+++ src/utilities/mle/MLE.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function MLEUtility() {}
+
+MLE.prototype.checkIsMLEForAPI = function checkIsMLEForAPI(merchantConfig, isMLESupportedByCybsForApi, operationId) {
+    //isMLE for an api is false by default
+    var isMLEForAPI=false;
+
+    //check here isMLE True or False
+    //if API is part of MLE then add the isMLE global paramter
+    if(isMLESupportedByCybsForApi === true && merchantConfig.isMLE === true){
+      isMLEForAPI=true;
+    }
+
+    //Control the MLE only from map
+    if(operationId in merchantConfig.mapToControlMLEonAPI){
+        if(merchantConfig.mapToControlMLEonAPI[operationId] === true){
+          isMLEForAPI=true;
+        }
+        if(merchantConfig.mapToControlMLEonAPI[operationId] === false){
+          isMLEForAPI=false;
+        }
+    }
+
+    return isMLEForAPI;
+}
+
+module.exports = MLEUtility;
