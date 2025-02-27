4d7f6fb4e0f5424ac3eb07c138ee87c98ce8221e
gnongsie <gnongsie@visa.com>
2025-02-27 16:13:04 +0530

    Changes from API updates - Feb 2025

---
+++ docs/TmsBusinessInformationAcquirer.md
@@ -0,0 +1,9 @@
+# CyberSource.TmsBusinessInformationAcquirer
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**acquirerId** | **String** | Acquirer ID; required unless both businessIdentificationType and businessIdentificationValue are provided.  | [optional] 
+**acquirerMerchantId** | **String** | Acquirer merchant ID; required unless both businessIdentificationType and businessIdentificationValue are provided.  | [optional] 
+
+
--- src/model/GenerateUnifiedCheckoutCaptureContextRequest.js
+++ src/model/GenerateUnifiedCheckoutCaptureContextRequest.js
@@ -16,18 +16,18 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/Upv1capturecontextsCaptureMandate', 'model/Upv1capturecontextsOrderInformation'], factory);
+    define(['ApiClient', 'model/Microformv2sessionsTransientTokenResponseOptions', 'model/Upv1capturecontextsCaptureMandate', 'model/Upv1capturecontextsOrderInformation'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('./Upv1capturecontextsCaptureMandate'), require('./Upv1capturecontextsOrderInformation'));
+    module.exports = factory(require('../ApiClient'), require('./Microformv2sessionsTransientTokenResponseOptions'), require('./Upv1capturecontextsCaptureMandate'), require('./Upv1capturecontextsOrderInformation'));
   } else {
     // Browser globals (root is window)
     if (!root.CyberSource) {
       root.CyberSource = {};
     }
-    root.CyberSource.GenerateUnifiedCheckoutCaptureContextRequest = factory(root.CyberSource.ApiClient, root.CyberSource.Upv1capturecontextsCaptureMandate, root.CyberSource.Upv1capturecontextsOrderInformation);
+    root.CyberSource.GenerateUnifiedCheckoutCaptureContextRequest = factory(root.CyberSource.ApiClient, root.CyberSource.Microformv2sessionsTransientTokenResponseOptions, root.CyberSource.Upv1capturecontextsCaptureMandate, root.CyberSource.Upv1capturecontextsOrderInformation);
   }
-}(this, function(ApiClient, Upv1capturecontextsCaptureMandate, Upv1capturecontextsOrderInformation) {
+}(this, function(ApiClient, Microformv2sessionsTransientTokenResponseOptions, Upv1capturecontextsCaptureMandate, Upv1capturecontextsOrderInformation) {
   'use strict';
 
 
@@ -46,6 +46,7 @@
    */
   var exports = function() {
     var _this = this;
+
 
 
 
@@ -92,6 +93,9 @@
       if (data.hasOwnProperty('orderInformation')) {
         obj['orderInformation'] = Upv1capturecontextsOrderInformation.constructFromObject(data['orderInformation']);
       }
+      if (data.hasOwnProperty('transientTokenResponseOptions')) {
+        obj['transientTokenResponseOptions'] = Microformv2sessionsTransientTokenResponseOptions.constructFromObject(data['transientTokenResponseOptions']);
+      }
     }
     return obj;
   }
@@ -134,6 +138,10 @@
    * @member {module:model/Upv1capturecontextsOrderInformation} orderInformation
    */
   exports.prototype['orderInformation'] = undefined;
+  /**
+   * @member {module:model/Microformv2sessionsTransientTokenResponseOptions} transientTokenResponseOptions
+   */
+  exports.prototype['transientTokenResponseOptions'] = undefined;
 
 
 
